69d4d3c1f310c3fc28d1fe317a39ea7d27269d99
dennisleprozo <dennisvleprozo@gmail.com>
2018-10-09 14:36:26 -0600

    final

--- practice.js
+++ practice.js
@@ -189,6 +189,8 @@ var users = [
   },
 ];
 
-
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+});
 // Do not edit the code above.
 
